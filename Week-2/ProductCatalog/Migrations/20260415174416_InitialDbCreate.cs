a93b5bbe3d0a0550481c4ba00d0b64fd237fd7f4
jdelacruz96 <jonathan.delacruz@revature.com>
2026-04-15 14:43:42 -0400

    finished EF day 1 demo

---
+++ Week-2/ProductCatalog/Migrations/20260415174416_InitialDbCreate.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductCatalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDbCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    TagId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.TagId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(10, 2)", nullable: false),
+                    Stock = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductTag",
+                columns: table => new
+                {
+                    ProductsProductId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TagsTagId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTag", x => new { x.ProductsProductId, x.TagsTagId });
+                    table.ForeignKey(
+                        name: "FK_ProductTag_Products_ProductsProductId",
+                        column: x => x.ProductsProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductTag_Tags_TagsTagId",
+                        column: x => x.TagsTagId,
+                        principalTable: "Tags",
+                        principalColumn: "TagId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductTag_TagsTagId",
+                table: "ProductTag",
+                column: "TagsTagId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductTag");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
--- Week-2/ProductCatalog/Program.cs
+++ Week-2/ProductCatalog/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ProductCatalog.Data;
+using ProductCatalog.Models;
+
+Console.WriteLine("Hello, World!");
+
+Category books = new Category
+{
+  Name = "Books",
+  Description = "Books and book accesories."  
+};
+
+Category electronics = new Category
+{
+  Name = "Electronics",
+  Description = "Electronics and computer stuff."  
+};
+
+// For now, we will manually create our AppDbContext object
+// Once we get to ASP, we'll let the dependency manager handle all that
+var context = new AppDbContext(); //don't misuse var 
+
+// Telling EF Core we want to add these objects as rows in our Categories table
+context.Categories.Add(books);
+context.Categories.Add(electronics);
+
+Console.WriteLine("Created my objects... didn't save yet");
+
+// Nothing get's written until...
+context.SaveChanges();
+
+Console.WriteLine("Saved changes, check db!");
+
+context.Categories.Remove(books);
+context.Categories.Remove(electronics);
+
+Console.WriteLine("removed books category");
+
+context.SaveChanges();
+
+// Reading from my db
+Category foundCategory = context.Categories.Find(2);
+
+Console.WriteLine(foundCategory.Description);
