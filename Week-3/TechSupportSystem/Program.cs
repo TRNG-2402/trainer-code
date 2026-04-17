5ae9c194092fbb8f75e7dd2308c7b0ae3fd02bd5
jdelacruz96 <jonathan.delacruz@revature.com>
2026-04-17 10:33:48 -0400

    created TechSupportSystem skeleton

---
+++ Week-3/TechSupportSystem/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(); // Adding Swagger
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+//Adding swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); // Serves the JSON at /swagger/v1/swagger.json
+    app.UseSwaggerUI(); // Serves the UI at /swagger
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
