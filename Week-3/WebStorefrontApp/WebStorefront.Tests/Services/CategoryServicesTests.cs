4a44fb2926d4c5bfee97f56a8909fd98c6aebebc
jdelacruz96 <jonathan.delacruz@revature.com>
2026-04-24 10:20:23 -0400

    added unit testing with mock

--- Week-3/WebStorefrontApp/WebStorefront.Tests/Services/CategoryServicesTests.cs
+++ Week-3/WebStorefrontApp/WebStorefront.Tests/Services/CategoryServicesTests.cs
@@ -7,6 +7,12 @@ using ProductCatalog.Services;
 
 namespace WebStorefront.Tests.Services;
 
+//https://learn.microsoft.com/en-us/dotnet/core/testing/unit-testing-code-coverage?tabs=windows
+
+// Follow the above guide to generate a test coverage report
+// That you can view in your browser like a website, focus only on getting 20% branch coverage 
+// in your service classes. Ignore everything else. 
+
 public class CategoryServiceTests
 {
     //Moq lets us create fake objects so we can avoid calling their real code
@@ -28,6 +34,27 @@ public class CategoryServiceTests
 
         // Using those mock objects to satisfy the CategoryService Constructor
         _sut = new CategoryService(_repoMock.Object, _cacheMock.Object);//when we create this category service object, we use our Mock
+    }
+
+    [Fact]
+    public async Task DeleteCategoryAsync_InvalidId_ThrowsAndNeverTouchesRepoOrCache()
+    {
+        // Allocate/Arrange
+        //Allocation is done, we already have our objects arranged for us
+
+        //Act + Assert
+        await Assert.ThrowsAsync<ArgumentOutOfRangeException>(
+            () => _sut.DeleteCategoryAsync(0) // When this method fires off with this input value, 
+        );                                      // the OutOfRange exception is thrown
+
+        // In addition to testing whether or not we threw the correct exception,
+        // we can assert that certain methods fired x amount of times
+        // in this case, none at all
+        _repoMock.Verify(r => r.GetCategoryByIdAsync(It.IsAny<int>()), Times.Never);
+        _repoMock.Verify(r => r.DeleteCategoryAsync(It.IsAny<Category>()), Times.Never);
+
+        // Cache was also never touched
+        _cacheMock.Verify(r => r.Remove(It.IsAny<Object>()), Times.Never);
     }
 
 }
